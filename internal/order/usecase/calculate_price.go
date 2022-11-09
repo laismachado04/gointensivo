514b04cd219bbb9c78e32112830314014c438fe7
Lais Machado <laislmo@gmail.com>
2022-11-09 17:20:53 -0300

    aula 1

---
+++ internal/order/usecase/calculate_price.go
@@ -0,0 +1,53 @@
+package usecase
+
+import (
+	"github.com/devfullcycle/gointensivo/internal/order/entity"
+	"github.com/devfullcycle/gointensivo/internal/order/infra/database"
+)
+
+type OrderInputDTO struct {
+	ID    string
+	Price float64
+	Tax   float64
+}
+
+type OrderOutputDTO struct {
+	ID         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+type CalculateFinalPriceUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewCalculateFinalPriceUseCase(orderRepository database.OrderRepository) *CalculateFinalPriceUseCase {
+	return &CalculateFinalPriceUseCase{
+		OrderRepository: &orderRepository,
+	}
+}
+
+func (u *CalculateFinalPriceUseCase) Execute(input OrderInputDTO) (*OrderOutputDTO, error) {
+	order, err := entity.NewOrder(input.ID, input.Price, input.Tax)
+	if err != nil {
+		return nil, err
+	}
+
+	err = order.CalculateFinalPrice()
+	if err != nil {
+		return nil, err
+	}
+
+	err = u.OrderRepository.Save(order)
+	if err != nil {
+		return nil, err
+	}
+
+	return &OrderOutputDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}, nil
+}
